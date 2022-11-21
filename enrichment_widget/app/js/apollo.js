45c35777804fbf5c33bb6a7e08476a09de8c0eba
Patrick Toepel <ptoepel@outsource-consultants.com>
2022-11-21 13:00:36 -0600

    apollo script dummy data

--- enrichment_widget/app/js/apollo.js
+++ enrichment_widget/app/js/apollo.js
@@ -1,15 +1,27 @@
 /*delete test data*/
-var company = "";
-var email = "";
-var lastName = "";
-var firstName = "";
+var company = "Apollo";
+var email = "name@domain.io";
+var lastName = "Zheng";
+var firstName = "Tim";
+var domain = "apollo.io";
 
+/*
+{
+    "api_key": "CimAYp3p4OegDg3MRj-MRA",
+    "first_name": "Tim",
+    "last_name": "Zheng",
+    "organization_name": "Apollo",
+    "email": "name@domain.io",
+    "domain": "apollo.io"
+}
+*/
 myBody = {
     "api_key":"CimAYp3p4OegDg3MRj-MRA",
     "organization_name":company,
     "email":email,
     "last_name":lastName,
     "first_name":firstName,
+    "domain": domain
 
 }
 
