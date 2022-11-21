bc990f3935242fce0066abb8bdde4f721811c939
Patrick Toepel <ptoepel@outsource-consultants.com>
2022-11-21 13:57:49 -0600

    cors update

--- enrichment_widget/app/js/apollo.js
+++ enrichment_widget/app/js/apollo.js
@@ -1,7 +1,6 @@
 var myHeaders = new Headers();
 myHeaders.append("Cache-Control", "no-cache");
 myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Cookie", "GCLB=CNju87aTz5qrYg; X-CSRF-TOKEN=TFGSdQ3S9eFyc_LoSNSbFU6AFt9qSaejhA8DdJ8DF_2606y5jOIX0IceEVAJOFy8uconv_c65Trd91LtPq2nUQ; _leadgenie_session=qbQliDiouzEOpr3tdC7hYARaNr%2FTfXMVCUQVs8CGbPw%2BgMYr0nPbob0C%2Bx9pKw%2FD9PQwHT5rX3mD%2F05FYM6JBtEfSoON79snNXmQXRMQnWf5HtpSSbALXjHSqRtilP18bcPvxDTu6tkizIUGszOHtk2f90gCZaF6vwSvXM1SEsxWqRmg%2FE8DCLNZdRrbiYpKLICoirpLzdQomdyBfXbDHVHQPipbyg8nl8fIV9eevHjxPg6GzXmwsSmaHyCapzVcjZMW2J%2BfEvAsSwX%2BuE9nX4SPIaMFTqe%2BiF8%3D--UXzOVGMHyvAyEnnn--M2V44zDdrYTyXS5%2FK9Ci%2FQ%3D%3D");
 
 var raw = JSON.stringify({
   "api_key": "CimAYp3p4OegDg3MRj-MRA",
@@ -15,8 +14,7 @@ var raw = JSON.stringify({
 var requestOptions = {
   method: 'POST',
   headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
+  body: raw
 };
 
 fetch("https://api.apollo.io/v1/people/match", requestOptions)
