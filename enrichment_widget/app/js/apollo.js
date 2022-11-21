eba389cfaf95b43f6b64a454e3038af6b60035ce
Patrick Toepel <ptoepel@outsource-consultants.com>
2022-11-21 13:38:51 -0600

    cors update

--- enrichment_widget/app/js/apollo.js
+++ enrichment_widget/app/js/apollo.js
@@ -1,47 +1,25 @@
-/*delete test data*/
-var company = "Apollo";
-var email = "name@domain.io";
-var lastName = "Zheng";
-var firstName = "Tim";
-var domain = "apollo.io";
+var myHeaders = new Headers();
+myHeaders.append("Cache-Control", "no-cache");
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Cookie", "GCLB=CNju87aTz5qrYg; X-CSRF-TOKEN=TFGSdQ3S9eFyc_LoSNSbFU6AFt9qSaejhA8DdJ8DF_2606y5jOIX0IceEVAJOFy8uconv_c65Trd91LtPq2nUQ; _leadgenie_session=qbQliDiouzEOpr3tdC7hYARaNr%2FTfXMVCUQVs8CGbPw%2BgMYr0nPbob0C%2Bx9pKw%2FD9PQwHT5rX3mD%2F05FYM6JBtEfSoON79snNXmQXRMQnWf5HtpSSbALXjHSqRtilP18bcPvxDTu6tkizIUGszOHtk2f90gCZaF6vwSvXM1SEsxWqRmg%2FE8DCLNZdRrbiYpKLICoirpLzdQomdyBfXbDHVHQPipbyg8nl8fIV9eevHjxPg6GzXmwsSmaHyCapzVcjZMW2J%2BfEvAsSwX%2BuE9nX4SPIaMFTqe%2BiF8%3D--UXzOVGMHyvAyEnnn--M2V44zDdrYTyXS5%2FK9Ci%2FQ%3D%3D");
 
-/*
-{
-    "api_key": "CimAYp3p4OegDg3MRj-MRA",
-    "first_name": "Tim",
-    "last_name": "Zheng",
-    "organization_name": "Apollo",
-    "email": "name@domain.io",
-    "domain": "apollo.io"
-}
-*/
+var raw = JSON.stringify({
+  "api_key": "CimAYp3p4OegDg3MRj-MRA",
+  "first_name": "Tim",
+  "last_name": "Zheng",
+  "organization_name": "Apollo",
+  "email": "name@domain.io",
+  "domain": "apollo.io"
+});
 
-myBody = {
-    "api_key":"CimAYp3p4OegDg3MRj-MRA",
-    "organization_name":company,
-    "email":email,
-    "last_name":lastName,
-    "first_name":firstName,
-    "domain": domain
-
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
 };
 
-    fetch("https://api.apollo.io/v1/people/match", 
-        {
-            method: "POST", 
-            body: myBody,
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'Cache-Control':'no-cache'
-            }
-        }
-    ).then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch((err) => {
-         console.log(err);
-        })
-
-
+fetch("https://api.apollo.io/v1/people/match", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
