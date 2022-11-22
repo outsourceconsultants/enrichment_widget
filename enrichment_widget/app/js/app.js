3954463a4d3571f474f0b67cd44181be78344991
Patrick Toepel <ptoepel@outsource-consultants.com>
2022-11-22 12:37:48 -0600

    update JS SDK post request

--- enrichment_widget/app/js/app.js
+++ enrichment_widget/app/js/app.js
@@ -21,28 +21,6 @@ ZOHO.CRM.API.getRecord({Entity: module, RecordID:id})
 
     // This is the information about the current record, if applicable.
 
-       var data ={
-        api_key: "CimAYp3p4OegDg3MRj-MRA",
-        first_name: "Tim",
-        last_name: "Zheng",
-        organization_name: "Apollo",
-        email: "name@domain.io",
-        domain: "apollo.io"
-        };
-        var request ={
-             url : api_url,
-
-             headers:{
-                "Content-Type": "application/json",
-                "Cache-Control": "no-cache"
-             },
-             body:data
-        }
-        ZOHO.CRM.HTTP.post(request)
-        .then(function(data){
-            console.log(data)
-        })
-
 
 
 
@@ -53,4 +31,27 @@ ZOHO.CRM.API.getRecord({Entity: module, RecordID:id})
 
 });
 
+
+
+var data ={
+    api_key: "CimAYp3p4OegDg3MRj-MRA",
+    first_name: "Tim",
+    last_name: "Zheng",
+    organization_name: "Apollo",
+    email: "name@domain.io",
+    domain: "apollo.io"
+    };
+    var request ={
+         url : api_url,
+
+         headers:{
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache"
+         },
+         body:data
+    }
+    ZOHO.CRM.HTTP.post(request)
+    .then(function(data){
+        console.log(data);
+    });
 ZOHO.embeddedApp.init();
